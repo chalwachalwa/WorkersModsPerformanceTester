168ff747eba71f21e84d590ddba9da3d64182cb6
chalwa <chalwachalwa@proton.me>
2023-02-26 15:50:05 +0100

    Initial commit

---
+++ WorkersModsPerformanceTester/CsvBuilder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkersModsPerformanceTester
+{
+    internal class CsvBuilder
+    {
+        private char _columnSeparator;
+        private string[] _columnsNames = new string[]{ };
+        private StringBuilder _stringBuilder = new StringBuilder();
+
+        public CsvBuilder(char columnSeparator = '\t')
+        {
+            _columnSeparator = columnSeparator;
+        }
+
+        public void SetUpColumns(params string[] columnsNames)
+        {
+            if(columnsNames == null)
+            {
+                throw new ArgumentNullException(nameof(columnsNames));
+            }
+            if(columnsNames.Length == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(columnsNames));
+            }
+            if(_columnsNames.Length > 0 || _stringBuilder.Length > 0)
+            {
+                throw new InvalidOperationException();
+            }
+            BuildRow(columnsNames);
+        }
+
+        public void AddRow(params string[] records)
+        {
+            BuildRow(records);
+        }
+
+        public override string ToString()
+        {
+            return _stringBuilder.ToString();
+        }
+
+        private void BuildRow(string[] records)
+        {
+            for (int i = 0; i < records.Length; i++)
+            {
+                _stringBuilder.Append(records[i]);
+                if (i != records.Length - 1)
+                {
+                    _stringBuilder.Append(_columnSeparator);
+                }
+            }
+            _stringBuilder.Append('\n');
+        }
+    }
+}
---
+++ WorkersModsPerformanceTester/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+
+namespace WorkersModsPerformanceTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var csvBuilder = new CsvBuilder();
+            csvBuilder.SetUpColumns("Mod number", "Mod name\\submod", "Lod files", "Path", "Warnings");
+            
+            var workshopPath = "C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\\784150";
+            var modFolders = Directory.GetDirectories(workshopPath);
+
+            int i = 1;
+
+            // map      - WORKSHOP_ITEMTYPE_LANDSCAPE
+            // vehicle  - WORKSHOP_ITEMTYPE_VEHICLE
+            // building - WORKSHOP_ITEMTYPE_BUILDING
+            var modTypes = new[] { "WORKSHOP_ITEMTYPE_BUILDING", "WORKSHOP_ITEMTYPE_VEHICLE" };
+
+            foreach (var modFolder in modFolders)
+            {
+                Console.WriteLine($"{i++}/{modFolders.Length}");
+                var modNumber = Path.GetFileName(Path.GetDirectoryName(modFolder));
+                string modName;
+                string modType;
+                
+                try
+                {
+                    modName = GetModName(modFolder, out modType);
+                }
+                catch(ApplicationException e)
+                {
+                    csvBuilder.AddRow(modNumber,"","","", modFolder, e.Message);
+                    continue;
+                }
+
+                if (!modTypes.Contains(modType)) continue;
+
+                var subfolders = Directory.GetDirectories(modFolder);
+                foreach(var subfolder in subfolders)
+                {
+                    var appendSubmod = subfolders.Length < 1;
+                     
+                    var filesWithLod = Directory.GetFiles(subfolder, "*.nmf").Where(x => x.Contains("LOD", StringComparison.InvariantCultureIgnoreCase)).Count();
+
+                    csvBuilder.AddRow(modNumber, modName, filesWithLod.ToString(), subfolder);
+                };
+            }
+
+            Console.Write(csvBuilder.ToString());
+        }
+
+        private static string GetModName(string modPath, out string modType)
+        {
+            string modTypeValue = null;
+
+            string configPath = Path.Combine(modPath, "workshopconfig.ini");
+            try
+            {
+                using (var fileReader = new StreamReader(configPath))
+                {
+                    while (!fileReader.EndOfStream)
+                    {
+                        var line = fileReader.ReadLine();
+                        if (line.Contains("$ITEM_TYPE"))
+                        {
+                            modTypeValue = line.Replace("\"", "").Replace("$ITEM_TYPE", "").Trim();
+                        }
+
+                        if (line.Contains("$ITEM_NAME"))
+                        {
+                            modType = string.IsNullOrEmpty(modTypeValue) ? "Invalid" : modTypeValue;
+                            return line.Replace("\"", "").Replace("$ITEM_NAME", "").Trim();
+                        }
+                    }
+                }
+            }
+            catch(FileNotFoundException e)
+            {
+                throw new ApplicationException("Invalid mod - no workshopconfig.ini", e);
+            }
+            throw new ApplicationException("Invalid mod - no name");
+        }
+    }
+}
